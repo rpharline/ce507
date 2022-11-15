8d5de0935179b4449dbedbb7a4aec35e6338de40
rpharline <rpharline@gmail.com>
2022-11-14 23:34:59 -0700

    updated mesh

--- Mesh_Generation.py
+++ Mesh_Generation.py
@@ -160,7 +160,71 @@ class Test_generateMesh1D( unittest.TestCase ):
         self.assertIsInstance( obj = ien_array, cls = numpy.ndarray )
         self.assertTrue( numpy.allclose( node_coords, gold_node_coords ) )
         self.assertTrue( numpy.array_equiv( ien_array, gold_ien_array ) )
-        
+ 
+class Test_generateMesh( unittest.TestCase ):
+    def test_make_1_linear_elem( self ):
+        gold_node_coords = numpy.array( [ 0.0, 1.0 ] )
+        gold_connect = { 0: [ 0, 1 ] }
+        node_coords, connect = generateMesh( xmin = 0.0, xmax = 1.0, degree = [ 1 ] )
+        self.assertIsInstance( obj = node_coords, cls = numpy.ndarray )
+        self.assertIsInstance( obj = connect, cls = dict )
+        self.assertTrue( numpy.allclose( node_coords, gold_node_coords ) )
+        self.assertDictEqual( d1 = gold_connect, d2 = connect )
+
+    def test_make_1_quadratic_elem( self ):
+        gold_node_coords = numpy.array( [ 0.0, 0.5, 1.0 ] )
+        gold_connect = { 0: [ 0, 1, 2 ] }
+        node_coords, connect = generateMesh( xmin = 0.0, xmax = 1.0, degree = [ 2 ] )
+        self.assertIsInstance( obj = node_coords, cls = numpy.ndarray )
+        self.assertIsInstance( obj = connect, cls = dict )
+        self.assertTrue( numpy.allclose( node_coords, gold_node_coords ) )
+        self.assertDictEqual( d1 = gold_connect, d2 = connect )
+    
+    def test_make_2_linear_elems( self ):
+        gold_node_coords = numpy.array( [ 0.0, 0.5, 1.0 ] )
+        gold_connect = { 0: [ 0, 1 ], 1: [ 1, 2 ] }
+        node_coords, connect = generateMesh( xmin = 0.0, xmax = 1.0, degree = [ 1, 1 ] )
+        self.assertIsInstance( obj = node_coords, cls = numpy.ndarray )
+        self.assertIsInstance( obj = connect, cls = dict )
+        self.assertTrue( numpy.allclose( node_coords, gold_node_coords ) )
+        self.assertDictEqual( d1 = gold_connect, d2 = connect )
+    
+    def test_make_2_quadratic_elems( self ):
+        gold_node_coords = numpy.array( [ 0.0, 0.25, 0.5, 0.75, 1.0 ] )
+        gold_connect = { 0: [ 0, 1, 2 ], 1: [ 2, 3, 4 ] }
+        node_coords, connect = generateMesh( xmin = 0.0, xmax = 1.0, degree = [ 2, 2 ] )
+        self.assertIsInstance( obj = node_coords, cls = numpy.ndarray )
+        self.assertIsInstance( obj = connect, cls = dict )
+        self.assertTrue( numpy.allclose( node_coords, gold_node_coords ) )
+        self.assertDictEqual( d1 = gold_connect, d2 = connect )
+    
+    def test_make_4_linear_elems( self ):
+        gold_node_coords = numpy.array( [ 0.0, 0.25, 0.5, 0.75, 1.0 ] )
+        gold_connect = { 0: [ 0, 1 ], 1: [ 1, 2 ], 2: [ 2, 3 ], 3: [ 3, 4 ] }
+        node_coords, connect = generateMesh( xmin = 0.0, xmax = 1.0, degree = [ 1, 1, 1, 1 ] )
+        self.assertIsInstance( obj = node_coords, cls = numpy.ndarray )
+        self.assertIsInstance( obj = connect, cls = dict )
+        self.assertTrue( numpy.allclose( node_coords, gold_node_coords ) )
+        self.assertDictEqual( d1 = gold_connect, d2 = connect )
+    
+    def test_make_4_quadratic_elems( self ):
+        gold_node_coords = numpy.array( [ 0.0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0 ] )
+        gold_connect = { 0: [ 0, 1, 2 ], 1: [ 2, 3, 4 ], 2: [ 4, 5, 6 ], 3: [ 6, 7, 8 ] }
+        node_coords, connect = generateMesh( xmin = 0.0, xmax = 1.0, degree = [ 2, 2, 2, 2 ] )
+        self.assertIsInstance( obj = node_coords, cls = numpy.ndarray )
+        self.assertIsInstance( obj = connect, cls = dict )
+        self.assertTrue( numpy.allclose( node_coords, gold_node_coords ) )
+        self.assertDictEqual( d1 = gold_connect, d2 = connect )
+    
+    def test_make_4_p_refine_elems( self ):
+        gold_node_coords = numpy.array( [ 0.0, 1.0, 1.5, 2.0, (2.0 + 1.0/3.0), (2.0 + 2.0/3.0), 3.0, 3.25, 3.5, 3.75, 4.0 ] )
+        gold_connect = { 0: [ 0, 1 ], 1: [ 1, 2, 3 ], 2: [ 3, 4, 5, 6 ], 3: [ 6, 7, 8, 9, 10 ] }
+        node_coords, connect = generateMesh( xmin = 0.0, xmax = 4.0, degree = [ 1, 2, 3, 4 ] )
+        self.assertIsInstance( obj = node_coords, cls = numpy.ndarray )
+        self.assertIsInstance( obj = connect, cls = dict )
+        self.assertTrue( numpy.allclose( node_coords, gold_node_coords ) )
+        self.assertDictEqual( d1 = gold_connect, d2 = connect )
+    
 # class Test_computeSolution( unittest.TestCase ):
 #     def test_single_linear_element_poly( self ):
 #         test_solution, node_coords, ien_array = computeSolution( target_fun = lambda x : x, domain = [-1.0, 1.0 ], num_elems = 1, degree = 1 )
@@ -213,4 +277,4 @@ class Test_generateMesh1D( unittest.TestCase ):
 #         self.assertAlmostEqual( first = evaluateSolutionAt( x = +1.0, coeff = coeff, node_coords = node_coords, ien_array = ien_array, eval_basis = basis.evalLagrangeBasis1D ), second = +0.25 )
 #         self.assertAlmostEqual( first = evaluateSolutionAt( x = +2.0, coeff = coeff, node_coords = node_coords, ien_array = ien_array, eval_basis = basis.evalLagrangeBasis1D ), second = +1.00 )
 
-# unittest.main()
+unittest.main()
